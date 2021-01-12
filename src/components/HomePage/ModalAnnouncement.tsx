f08374a88f17b1d9c26dfb62445819283353fa03
TrueDollar <truedollar@protonmail.com>
2021-01-12 16:41:00 +0700

    update ann

--- src/components/HomePage/ModalAnnouncement.tsx
+++ src/components/HomePage/ModalAnnouncement.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {Modal, Button, useTheme} from '@aragon/ui';
 import {useHistory} from 'react-router-dom';
+import {COUPON_EXPIRATION} from "../../constants/values";
 
 function ModalAnnouncement({show, onClose}: { show: boolean, onClose: Function }) {
   const theme = useTheme();
@@ -19,14 +20,19 @@ function ModalAnnouncement({show, onClose}: { show: boolean, onClose: Function }
           padding: 5
         }}>
           <div>
-            <p>At Epoch 321 (81 epochs from Bootstrap end). We will do a Snapshot of coupons holders.</p>
+            <p>At Epoch 340 (100 epochs from Bootstrap end). We will do a Snapshot of coupons holders.</p>
 
             <p className="my-2">
               Binance Smart Chain Implementation preallocation tokens (total 3,000 $TSD tokens) will be distributed only
-              for coupon holders based on % of coupons holding on total. Weight = 321/epochofCouponPurchase.
+              for coupon holders based on % of coupons holding on total. Weight = 340/epochofCouponPurchase.
             </p>
 
             <p>Let's go cross-chain!!!!</p>
+            <a
+              style={{
+                color: theme.warningSurfaceContent
+              }}
+              href="https://twitter.com/TrueSeigniorage/status/1348925598047997955?s=20" target='_blank'>View more</a>
           </div>
 
         </div>
